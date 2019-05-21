226cdf9fa9f9cdbf5d08036f4bb2ee7f72ff4eaf
AdoenLunnae <unshekdelmonton@gmail.com>
2019-05-21 14:41:02 +0200

    Finished e5, started e6

--- p4/ejercicio5.sh
+++ p4/ejercicio5.sh
@@ -1,6 +1,10 @@
 cat /proc/cpuinfo | sed -r -n '1,/^[ \t]*$/p' | sed -r -n '
-	s/model name[ \t]+: +(.+)/ Modelo: \1/p;
-	s/cpu MHz[ \t]+: ([0-9\.]+)/ Megahercios: \1/p;
-	s/cpu cores [ \t]+: ([0-9]+)/ Número máximo de hilos/ 	
-	'
-
+	s/model name[ \t]+: +(.+)/Modelo: \1/p;
+	s/cpu MHz[ \t]+: ([0-9\.]+)/Megahercios: \1/p;
+	s/cpu cores[ \t]+: ([0-9]+)/Número máximo de hilos: \1/p; 	
+'
+cat /proc/cpuinfo | sed -nr "1,/^[ \t]*/s/vendor_id[ \t]+: (.+)/ID vendedor: \1/p"
+echo "Puntos de montaje:"
+cat /proc/mounts | sed -nr "s/^([^ ]+) ([^ ]+) ([^ ]+) .+/-> Punto de montaje: \2, Dispositivo: \1, Tipo de dispositivo: \3/p" | sort -k 5 -r -t " "
+echo "Particiones:"
+cat /proc/partitions | sed -nr "s/^ +[^ ]+ +[^ ]+ +([^ ]+) +([^ ]+)/-> Particion: \2, Numero de bloques: \1/p"
